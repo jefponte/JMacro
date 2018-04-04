f3dc1d16cff894d1f7434666552f055f97914061
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2018-04-04 11:28:07 -0300

    Uns tratamentos, entrada de @.

--- src/main/java/br/jmacro/controller/JMacro.java
+++ src/main/java/br/jmacro/controller/JMacro.java
@@ -2,6 +2,7 @@ package br.jmacro.controller;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
@@ -19,21 +20,19 @@ import java.util.Properties;
 
 import br.jmacro.view.JMacroView;
 
-
 public class JMacro {
 	public Robot robot;
 	private JMacroView view;
 	private ArrayList<String> comandos;
-	private ArrayList<Rotina>rotinas;
-	
-	
-	public JMacro(){
-		
+	private ArrayList<Rotina> rotinas;
+
+	public JMacro() {
+
 		rotinas = new ArrayList<>();
 		comandos = new ArrayList<>();
 		view = new JMacroView();
 		view.setVisible(false);
-		
+
 		try {
 			robot = new Robot();
 		} catch (AWTException e) {
@@ -43,10 +42,9 @@ public class JMacro {
 
 	}
 
-
-	public void carregar(String caminho){
+	public void carregar(String caminho) {
 		File arquivoVerifica = new File(caminho);
-		if(!arquivoVerifica.exists()){
+		if (!arquivoVerifica.exists()) {
 			FileWriter fw;
 			try {
 				fw = new FileWriter(arquivoVerifica, true);
@@ -75,12 +73,13 @@ public class JMacro {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	public void executarLista(){
+	public void executarLista() {
 		this.view.setVisible(true);
 		for (String comando : comandos) {
+			System.out.println("Executar " + comando);
 			executa(comando);
 			try {
 				Thread.sleep(300);
@@ -91,42 +90,54 @@ public class JMacro {
 		}
 		this.view.setVisible(false);
 	}
-	
+
 	/**
-	 * Processa um comando e executa. 
+	 * Processa um comando e executa.
+	 * 
 	 * @param comando
 	 */
-	public void executa(String mensagem){
+	public void executa(String mensagem) {
+
+		if (mensagem == null)
+			return;
+		if (mensagem.length() < 2) {
+			return;
+		}
+		if (mensagem.charAt(0) == '#') {
+			return;
+		}
 		if (mensagem.indexOf('(') != -1 && mensagem.indexOf(')') != -1) {
 			String comando = mensagem.substring(0, mensagem.indexOf('('));
-			String parametros = mensagem.substring((mensagem.indexOf('(') + 1),mensagem.indexOf(')'));
-			
-			if(comando.equals("click")){
+			String parametros = mensagem.substring((mensagem.indexOf('(') + 1), mensagem.indexOf(')'));
+
+			if (comando.equals("click")) {
 				
 				try {
-					int x = Integer.parseInt(parametros.substring(0, parametros.indexOf(',')));
-					int y = Integer.parseInt(parametros.substring(parametros.indexOf(',') + 1));
-					
+					int x = Integer.parseInt(parametros.substring(0, parametros.indexOf(',')).trim());
+					int y = Integer.parseInt(parametros.substring(parametros.indexOf(',') + 1).trim());
+
 					robot.mouseMove(x, y);
 					Thread.sleep(200);
 					robot.mousePress(InputEvent.BUTTON1_MASK);
 					Thread.sleep(100);
-					robot.mouseRelease(InputEvent.BUTTON1_MASK);		
+					robot.mouseRelease(InputEvent.BUTTON1_MASK);
 					Thread.sleep(500);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+				} catch (StringIndexOutOfBoundsException e) {
+					System.out.println("click exige dois parametros numéricos. ");
 				}
-				
-			}else if(comando.equals("digitar")){
-				for (int i=0; i<parametros.length(); i++) {
-					   char c = parametros.charAt(i);
-					   teclar(c);
+
+			} else if (comando.equals("digitar")) {
+				for (int i = 0; i < parametros.length(); i++) {
+					char c = parametros.charAt(i);
+					teclar(c);
 				}
-			}else if(comando.equals("enter")){
-				
+			} else if (comando.equals("enter")) {
+
 				try {
-					robot.keyPress(KeyEvent.VK_ENTER); 
+					robot.keyPress(KeyEvent.VK_ENTER);
 					Thread.sleep(200);
 					robot.keyRelease(KeyEvent.VK_ENTER);
 					Thread.sleep(200);
@@ -134,9 +145,8 @@ public class JMacro {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-			}
-			else if(comando.equals("maximizar")){
+
+			} else if (comando.equals("maximizar")) {
 				try {
 					Thread.sleep(200);
 					robot.keyPress(KeyEvent.VK_WINDOWS);
@@ -150,31 +160,52 @@ public class JMacro {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				
+
 			}
 
 		}
-		
-	}
-	public void teclar(char c){
 
-		
+	}
+
+	public void teclar(char c) {
+		boolean capsLigado = Toolkit.getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK);
+		if (capsLigado) {
+			Toolkit.getDefaultToolkit().setLockingKeyState(KeyEvent.VK_CAPS_LOCK, false);
+		}
+
 		try {
+			
 			Thread.sleep(100);
-			robot.keyPress(Character.toUpperCase(c)); 
-			Thread.sleep(8);
-			robot.keyRelease(Character.toUpperCase(c));
-			Thread.sleep(100);
+			if(c == '@') {
+				robot.keyPress(KeyEvent.VK_SHIFT);
+				robot.keyPress(KeyEvent.VK_2);
+				Thread.sleep(8);
+				robot.keyRelease(KeyEvent.VK_SHIFT);
+				Thread.sleep(100);	
+			}else {
+				if(Character.isUpperCase(c)) {
+					robot.keyPress(KeyEvent.VK_SHIFT);
+				}
+				robot.keyPress(Character.toUpperCase(c));
+				Thread.sleep(8);
+				robot.keyRelease(Character.toUpperCase(c));
+				Thread.sleep(100);	
+			
+				if(Character.isUpperCase(c)) {
+					robot.keyRelease(KeyEvent.VK_SHIFT);
+				}
+			}
+			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	
-	public void carregarRotina(){
 		
+		
+	}
+
+	public void carregarRotina() {
+
 		Properties config = new Properties();
 		FileInputStream file;
 		try {
@@ -186,70 +217,71 @@ public class JMacro {
 			}
 			file.close();
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 		}
 	}
-	public void verificandoRotina(){
+
+	public void verificandoRotina() {
 		Thread verificando = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				do{
+				do {
 					for (Rotina rotina : rotinas) {
-						System.out.println("Verificando rotina "+rotina.getMacroID());
-						
+						System.out.println("Verificando rotina " + rotina.getMacroID());
+
 						Calendar calAtual = Calendar.getInstance();
 						calAtual.setTime(new Date());
 						Calendar cal = rotina.getHora();
-						
-						int horaAtual =  calAtual.get(Calendar.HOUR_OF_DAY);
+
+						int horaAtual = calAtual.get(Calendar.HOUR_OF_DAY);
 						int minutoAtual = calAtual.get(Calendar.MINUTE);
 						int mesAtual = calAtual.get(Calendar.MONTH);
 						int diaAtual = calAtual.get(Calendar.DAY_OF_MONTH);
-						System.out.println("Minuto Atual: "+minutoAtual);
-						System.out.println("Minuto da Rotina: "+ cal.get(Calendar.MINUTE));
-						
-						if(horaAtual == cal.get(Calendar.HOUR_OF_DAY) && minutoAtual == cal.get(Calendar.MINUTE)){
+						System.out.println("Minuto Atual: " + minutoAtual);
+						System.out.println("Minuto da Rotina: " + cal.get(Calendar.MINUTE));
+
+						if (horaAtual == cal.get(Calendar.HOUR_OF_DAY) && minutoAtual == cal.get(Calendar.MINUTE)) {
 							System.out.println("Hora minuto igual");
-							if (!(mesAtual == cal.get(Calendar.MONTH) && diaAtual == cal.get(Calendar.DAY_OF_MONTH))){
+							if (!(mesAtual == cal.get(Calendar.MONTH) && diaAtual == cal.get(Calendar.DAY_OF_MONTH))) {
 								System.out.println("Data diferente, executar comandos");
 								cal.set(Calendar.MONTH, mesAtual);
 								cal.set(Calendar.DAY_OF_MONTH, diaAtual);
 								cal.set(Calendar.YEAR, calAtual.get(Calendar.YEAR));
 								carregar(rotina.getMacroID());
 								executarLista();
-								
+
 								SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
-								Date now = Calendar.getInstance().getTime(); 
+								Date now = Calendar.getInstance().getTime();
 								String dataFormatada = sdf.format(now);
-								
-								log("Execução de macro "+rotina.getMacroID()+" em "+dataFormatada, "log.txt");
-								
-							}else{
+
+								log("Execução de macro " + rotina.getMacroID() + " em " + dataFormatada, "log.txt");
+
+							} else {
 								System.out.println("Data igual, comando ja foi executado.");
-								System.out.println("Mes da Rotina:"+rotina.getHora().get(Calendar.MONTH));
-								
-								
+								System.out.println("Mes da Rotina:" + rotina.getHora().get(Calendar.MONTH));
+
 							}
-						}else{
-							System.out.println("Hora minuto difetrente");
-							
+						} else {
+							System.out.println("Hora minuto diferente");
+
 						}
 						System.out.println("-------------");
 					}
 					try {
-						Thread.sleep(20000);
-						
+						Thread.sleep(12000);
+
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}while(true);
-				
+				} while (true);
+
 			}
 		});
 		verificando.start();
 	}
+
 	public void log(String mensagem, String nomeArquivo) {
 		File arquivo = new File(nomeArquivo);
 		if (!arquivo.exists()) {
@@ -285,7 +317,8 @@ public class JMacro {
 			}
 
 		}
-	
+
 	}
+
 	public static final String ARQUIVO_ROTINA = "rotina.txt";
 }
